f5fd5b34c74db968bad5a4ff3bf2362a756d8bc7
mreinart <manfred@reinart.de>
2022-12-20 18:14:26 +0100

    Stodeus UltaBip - 2022 -- YYYY-MM-DD-XSD-UBHHHH-NN.igc 2022-12-16-XSD-UB2F17-02.igc -- see igc_fr_specification_2022_with_al7_2022-1-31.pdf A2.5.1 Long file name style. Long file name example: 2021-05-15-MMM-XXXXXX-01.IGC 2021-05-15 = date MMM = manufacturer's three-letter IGC identifier (see table on next page) XXXXXX = unique FR Serial ID (S/ID); 6 alphanumeric characters (see the note below) 01 = second flight of the day, 02 for second flight, etc. Note: The XXXXXX field has 6-characters, with hyphens at the start and end, with leading zeros if necessary to make up the 6 characters. It can also be used by a manufacturer to differentiate between different FR types, such as using the first characters to indicate each FR type. (AL7)

--- test.js
+++ test.js
@@ -39,6 +39,8 @@ const tests = [
   ['2013-01-08_flight_1.igc', data('2013-01-08', null, null, 1)],
   ['2019-08-19-XSD-GPB-01.igc', data('2019-08-19', 'XSD', 'GPB', 1)],
   ['2022-10-28-XSD-MRT-02.igc', data('2022-10-28', 'XSD', 'MRT', 2)],
+  ['2022-12-16-XSD-UBP-02.igc', data('2022-12-16', 'XSD', 'UBP', 2)],
+  ['2022-12-16-XSD-UB2F17-02.igc', data('2022-12-16', 'XSD', 'UB2F17', 2)],
   ['2022-07-11_13.02_Reinhardsmunster_S.igc', data('2022-07-11', null, null, null)],
   ['2022-10-28_16.38_Orensberg.igc', data('2022-10-28', null, null, null)],
 ];
